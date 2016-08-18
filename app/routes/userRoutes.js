54605d59b9d8c8de8d8f9a8b641919425872ca69
Michal Chwedczuk <czytelny@gmail.com>
2016-08-18 09:22:39 +0200

    added use strict, modify router to work with POST

--- app/routes/userRoutes.js
+++ app/routes/userRoutes.js
@@ -1,23 +1,24 @@
+"use strict";
+
 const express = require('express');
 const router = express.Router();
 const User = require('../models/userModel');
 
-router.get('/', function (req, res) {
-  res.send('GET handler for /users route');
-});
+router.route('/')
+  .get(function (req, res) {
+    res.send('GET handler for /users route');
+  })
+  .post(function (req, res) {
+    let dude = new User({
+      "name": "Dude",
+      "email": "dude@gmail.com",
+      "password": "lebowsky"
+    });
 
-router.post('/', function (req, res, next) {
-  let dude = new User({
-    name: "Dude",
-    email: "dude@gmail.com",
-    "password": "lebowsky"
+    dude.save(function (err) {
+      if (err) res.status(500).send('500');
+      res.json({ message: 'Dude created!' });
+    });
   });
-
-  dude.save(function (err) {
-    if (err) throw err;
-    console.log('Woah, just created Dude !');
-  });
-  next();
-});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
