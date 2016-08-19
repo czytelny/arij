ae2ed7936cbcd14e9ace3d7a9411089e6eb04f27
Michal Chwedczuk <czytelny@gmail.com>
2016-08-19 18:46:08 +0200

    Websockets example added

--- app/routes/userRoutes.js
+++ app/routes/userRoutes.js
@@ -1,24 +1,48 @@
 "use strict";
 
-const express = require('express');
-const router = express.Router();
-const User = require('../models/userModel');
+module.exports = function (io) {
+  const express = require('express');
+  const router = express.Router();
+  const User = require('../models/userModel');
 
-router.route('/')
-  .get(function (req, res) {
-    res.send('GET handler for /users route');
-  })
-  .post(function (req, res) {
-    let dude = new User({
-      "name": "Dude",
-      "email": "dude@gmail.com",
-      "password": "lebowsky"
+  router.route('/')
+    .get(function (req, res) {
+      res.send('GET handler for /users route');
+    })
+    .post(function (req, res) {
+      let dude = new User({
+        "name": "Dude",
+        "email": "dude@gmail.com",
+        "password": "lebowsky"
+      });
+
+      router.route('/')
+        .get(function (req, res) {
+          res.send('GET handler for /users route');
+        })
+        .post(function (req, res) {
+          let dude = new User({
+            "name": "Dude",
+            "email": "dude@gmail.com",
+            "password": "lebowsky"
+          });
+
+          dude.save(function (err) {
+            if (err) res.status(500).send('500');
+            res.json({message: 'Dude created!'});
+          });
+        });
     });
 
-    dude.save(function (err) {
-      if (err) res.status(500).send('500');
-      res.json({ message: 'Dude created!' });
+  // ---------websockets
+  io.on('connection', function (socket) {
+    console.log("web socket connection established");
+    socket.emit('news', {hello: 'world'});
+
+    socket.on('some event', function (data) {
+      console.log(data);
     });
   });
 
-module.exports = router;
+  return router;
+};
--- server.js
+++ server.js
@@ -3,9 +3,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
 const database = require('./config/databaseConfig');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIO.listen(server);
 const PORT = process.env.PORT || 3030;
 
 // ------- setup for all environments
@@ -20,11 +24,11 @@ mongoose.connect(database.url, function (err) {
 
 // ------- routes
 const routes = require('./app/routes/routes');
-const userRoute = require('./app/routes/userRoutes');
+const userRoutes = require('./app/routes/userRoutes')(io);
 app.use('/', routes);
-app.use('/users', userRoute);
+app.use('/users', userRoutes);
 
 
-app.listen(PORT, function () {
+server.listen(PORT, function () {
   console.log('Example app listening on port: ' + PORT);
 });
