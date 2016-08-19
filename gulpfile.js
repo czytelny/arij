42dd4ba53e5b7ef23accb31a90c1c7a063bd0193
Michal Chwedczuk <czytelny@gmail.com>
2016-08-19 23:09:58 +0200

    gulp-jasmine with gulp-watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
-var del = require('del');
-var gulp = require('gulp');
-var path = require('path');
-var runSequence = require('run-sequence');
+const del = require('del');
+const gulp = require('gulp');
+const path = require('path');
+const runSequence = require('run-sequence');
+const jasmine = require('gulp-jasmine');
+const watch = require('gulp-watch');
 
 var npmDependencies = [
   'socket.io-client/socket.io.js'
@@ -34,3 +36,12 @@ gulp.task('build:server', function () {
 gulp.task('build', function(){
   runSequence('clean', 'build:server', 'build:public');
 });
+
+gulp.task('tests', function() {
+  return gulp.watch('app/**/*.js', function () {
+    gulp.src('tests/**/*.js')
+    // gulp-jasmine works on filepaths so you can't have any plugins before it
+      .pipe(jasmine());
+  });
+  }
+);
