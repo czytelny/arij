d2d0af4474c193afebb14127c467aecf7961ae11
Michal Chwedczuk <czytelny@gmail.com>
2016-08-27 21:54:29 +0200

    reorganizing tests for user actions

--- app/actions/usersActions.spec.js
+++ app/actions/usersActions.spec.js
@@ -17,77 +17,85 @@ describe('User Actions', function () {
   const middlewares = [thunk];
   const mockStore = configureMockStore(middlewares);
 
-  describe("should create action:", function () {
+  describe("should create action", function () {
+    describe("add user", function () {
+      it('request', function () {
+        const user = {
+          name: 'xxx',
+          email: 'a123@wp.pl'
+        };
+        expect(actions.addUserRequest(user)).to.eql({
+          type: actionsTypes.ADD_USER_REQUEST,
+          user
+        });
+      });
 
-    it('add user request', function () {
-      const user = {
-        name: 'xxx',
-        email: 'a123@wp.pl'
-      };
-      expect(actions.addUserRequest(user)).to.eql({
-        type: actionsTypes.ADD_USER_REQUEST,
-        user
+      it('success', function () {
+        var body = {};
+        expect(actions.addUserRequestSuccess(body)).to.contain({
+          type: actionsTypes.ADD_USER_REQUEST_SUCCESS,
+          body
+        });
+      });
+
+      it('failure', function () {
+        const errorCode = 500;
+        const errorMessage = "internal server error";
+
+        expect(actions.addUserRequestFailure(errorCode, errorMessage))
+          .to.eql({
+          type: actionsTypes.ADD_USER_REQUEST_FAILURE,
+          code: errorCode,
+          msg: errorMessage
+        });
       });
     });
 
-    it('add user request success', function () {
-      var body = {};
-      expect(actions.addUserRequestSuccess(body)).to.contain({
-        type: actionsTypes.ADD_USER_REQUEST_SUCCESS,
-        body
+    describe("get user", function () {
+      it("request", function () {
+        let userId = '3';
+        expect(actions.getUserRequest(userId)).to.be.a('Function');
       });
-    });
 
-    it('add user request failed', function () {
-      const errorCode = 500;
-      const errorMessage = "internal server error";
+      it("request success", sinon.test(function () {
+        let objFromDB = {id: "2"};
+        const store = mockStore({});
+        const expectedActions = [actions.getUserRequestSuccess(objFromDB)];
+        this.stub(userService, 'findById').returns(when.resolve(objFromDB));
 
-      expect(actions.addUserRequestFailure(errorCode, errorMessage))
-        .to.eql({
-        type: actionsTypes.ADD_USER_REQUEST_FAILURE,
-        code: errorCode,
-        msg: errorMessage
+        return store
+          .dispatch(actions.getUserRequest("51bb793aca2ab77a3200000d"))
+          .then(() => expect(store.getActions()).to.eql(expectedActions));
+      }));
+
+      it("request failure", sinon.test(function () {
+        let errObj = {err: "500"};
+        const store = mockStore({});
+        const expectedActions = [actions.getUserRequestFailure(errObj)];
+        this.stub(userService, 'findById').returns(when.reject(errObj));
+
+        return store
+          .dispatch(actions.getUserRequest("51bb793aca2ab77a3200000d"))
+          .then(() => expect(store.getActions()).to.eql(expectedActions));
+      }));
+
+      it("success", function () {
+        const body = {id: 3};
+        expect(actions.getUserRequestSuccess(body))
+          .to.eql({
+          type: actionsTypes.GET_USER_REQUEST_SUCCESS,
+          body
+        })
       });
-    });
 
-    it("get user request success", sinon.test(function () {
-      let objFromDB = {id: "2"};
-      const store = mockStore({});
-      const expectedActions = [actions.getUserRequestSuccess(objFromDB)];
-      this.stub(userService, 'findById').returns(when.resolve(objFromDB));
-
-      return store
-        .dispatch(actions.getUserRequest("51bb793aca2ab77a3200000d"))
-        .then(() => expect(store.getActions()).to.eql(expectedActions));
-    }));
-
-    it("get user request failure", sinon.test(function () {
-      let errObj = {err: "500"};
-      const store = mockStore({});
-      const expectedActions = [actions.getUserRequestFailure(errObj)];
-      this.stub(userService, 'findById').returns(when.reject(errObj));
-
-      return store
-        .dispatch(actions.getUserRequest("51bb793aca2ab77a3200000d"))
-        .then(() => expect(store.getActions()).to.eql(expectedActions));
-    }));
-
-    it("get user request success", function () {
-      const body = {id: 3};
-      expect(actions.getUserRequestSuccess(body))
-        .to.eql({
-        type: actionsTypes.GET_USER_REQUEST_SUCCESS,
-        body
-      })
-    });
-
-    it("get user request failure", function () {
-      const err = {msg:'500'};
-      expect(actions.getUserRequestFailure(err))
-        .to.eql({
-        type: actionsTypes.GET_USER_REQUEST_FAILURE,
-        err
-      })
+      it("failure", function () {
+        const err = {msg:'500'};
+        expect(actions.getUserRequestFailure(err))
+          .to.eql({
+          type: actionsTypes.GET_USER_REQUEST_FAILURE,
+          err
+        })
+      });
     });
   });
 });
