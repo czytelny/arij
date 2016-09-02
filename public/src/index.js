440d453164e1ab0f369eadcb7cad3dcc070a832d
Michal Chwedczuk <czytelny@gmail.com>
2016-09-02 20:17:08 +0200

    improve npm scripts to handle public vendor and project files build separately

--- public/src/index.js
+++ public/src/index.js
@@ -3,9 +3,6 @@ var React = require('react');
 var socketIO = require('socket.io-client');
 var Chat = require('./Chat');
 
-
-
-
 var socket = socketIO.connect('http://localhost:3030');
 socket.emit("users:post", {name: 'czeslaw', email: 'czeslaw@wp.pl', password: 'aaa'});
 socket.on('users:post:response', function (msg) {
