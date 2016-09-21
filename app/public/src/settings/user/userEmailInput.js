a6af1c512f06a9dc19563cfed0e19669bce56d1f
Michal Chwedczuk <czytelny@gmail.com>
2016-09-21 21:56:13 +0200

    slight ui validation change

--- app/public/src/settings/user/userEmailInput.js
+++ app/public/src/settings/user/userEmailInput.js
@@ -4,12 +4,17 @@ const UserEmailInput = ({emailChangeHandler, errors}) =>
   <div className="row">
     <div className="col-sm-8">
       <label htmlFor="userEmail">Email</label>
-      <div className={"input-group " + (errors.get("required") || errors.get("format")
-        ? "has-error" : "")}>
+      <div className="input-group">
         <div className="input-group-addon">
           <span className="b"><strong>@</strong></span>
         </div>
         <input onBlur={emailChangeHandler} type="text" className="form-control" id="userEmail" placeholder="Email"/>
+
+        {errors.get("required") || errors.get("format") ?
+          <div className="has-error">
+            <span className="glyphicon glyphicon-remove form-control-feedback"/>
+          </div>
+          : null}
       </div>
     </div>
   </div>;
--- app/public/src/settings/user/userNameInput.js
+++ app/public/src/settings/user/userNameInput.js
@@ -4,11 +4,17 @@ const UserNameInput = ({nameChangeHandler, errors}) =>
   <div className="row">
     <div className="col-sm-8">
       <label htmlFor="userName">User name</label>
-      <div className={"input-group " + (errors.get("required") ? "has-error" : "")}>
+      <div className="input-group">
         <div className="input-group-addon">
           <i className="fa fa-child" aria-hidden="true"/>
         </div>
         <input onBlur={nameChangeHandler} type="text" className="form-control" id="userName" placeholder="User name"/>
+
+        {errors.get("required") ?
+          <div className="has-error">
+            <span className="glyphicon glyphicon-remove form-control-feedback"/>
+          </div>
+          : null}
       </div>
     </div>
   </div>;
