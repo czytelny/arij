66140ec1c2e89dae1947ff85a732ab3de98bd9cc
Michal Chwedczuk <czytelny@gmail.com>
2016-09-23 23:08:45 +0200

    Hashing user password with bcrypt

--- app/models/userModel.js
+++ app/models/userModel.js
@@ -1,6 +1,7 @@
 "use strict";
 
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 import modelDecoratorService from './../services/modelDecoratorService';
 
 const Schema = mongoose.Schema;
@@ -15,7 +16,18 @@ const userSchema = new Schema({
 
 userSchema.pre('save', function (next) {
   modelDecoratorService.addTimestamp.call(this);
-  next();
+  if (!this.isModified('password')) {
+    return next();
+  }
+
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(this.password, salt, (err, hash) => {
+      if (err) return next(err);
+      this.password = hash;
+      next();
+    });
+  });
+
 });
 
 export default mongoose.model('User', userSchema);
