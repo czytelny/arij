213b3f709283454a71082039a2c458e35f3f9430
Michal Chwedczuk <czytelny@gmail.com>
2016-11-16 20:59:27 +0100

    remove unncessesary db closing

--- app/database.js
+++ app/database.js
@@ -6,6 +6,4 @@ import logger from 'winston';
 module.exports = mongoose.connect(dbConfig.url, function (err) {
   if (err) throw Error(`Can't establish connection with MongoDB on: ${dbConfig.url}`);
   logger.info('Connection to MongoDB established');
-  process.on('SIGINT', db.close);
-  process.on('SIGTERM', db.close);
 });
