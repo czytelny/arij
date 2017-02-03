a11d9f3cae2df783476c0c638f0632ee702e7783
Maciej Pawluk <m.blizniak@gmail.com>
2017-02-03 09:28:14 +0100

    Added watch support for *.less files located in different directories

--- app/public/src/index.js
+++ app/public/src/index.js
@@ -17,22 +17,22 @@ import ProjectView from './settings/project/projectView';
 import ProjectRestListView from './settings/projects/projectList/ProjectRestListView';
 import MessagesComponent from './app/messages/messagesComponent';
 
-render(
-  <Provider store={store}>
-    <div>
-      <Router history={browserHistory}>
-        <Route path="/" component={MainView}/>
-        <Route path="/settings/users" component={UserListView}/>
-        <Route path="/settings/user" component={UserView}/>
-        <Route path="/settings/user/:userId" component={UserView}/>
-        <Route path="/settings/project" component={ProjectListView}/>
-        <Route path="/settings/project/new" component={ProjectView}/>
-        <Route path="/settings/projects" component={ProjectRestListView}/>
-      </Router>
-      <MessagesComponent />
-    </div>
-  </Provider>,
-  document.getElementById('app')
-);
+// render(
+//   <Provider store={store}>
+//     <div>
+//       <Router history={browserHistory}>
+//         <Route path="/" component={MainView}/>
+//         <Route path="/settings/users" component={UserListView}/>
+//         <Route path="/settings/user" component={UserView}/>
+//         <Route path="/settings/user/:userId" component={UserView}/>
+//         <Route path="/settings/project" component={ProjectListView}/>
+//         <Route path="/settings/project/new" component={ProjectView}/>
+//         <Route path="/settings/projects" component={ProjectRestListView}/>
+//       </Router>
+//       <MessagesComponent />
+//     </div>
+//   </Provider>,
+//   document.getElementById('app')
+// );
 
-// render(router, document.getElementById('app'));
+render(router, document.getElementById('app'));
