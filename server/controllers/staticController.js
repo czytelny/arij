a24fcb8e2c79940a627997f0be171984c7691e42
Michal Chwedczuk <czytelny@gmail.com>
2017-03-03 21:09:17 +0100

    client app connected with backend

--- server/controllers/staticController.js
+++ server/controllers/staticController.js
@@ -4,6 +4,14 @@ const path = require('path')
 const router = require('express').Router()
 const {isLoggedIn} = require('./../services/authChecker')
 
+router.get('/static/css/*', isLoggedIn, function (req, res) {
+  res.sendFile(path.resolve('client/' + req.path))
+})
+
+router.get('/static/js/*', isLoggedIn, function (req, res) {
+  res.sendFile(path.resolve('client/' + req.path))
+})
+
 router.get('*', isLoggedIn, function (req, res) {
   res.sendFile(path.resolve('client/index.html'))
 })
