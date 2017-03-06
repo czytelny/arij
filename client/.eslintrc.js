6fbec18191569da86c9ef15313e6623e60cacf73
Michal Chwedczuk <czytelny@gmail.com>
2017-03-06 22:27:03 +0100

    eslint tweaks

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -36,6 +36,11 @@ module.exports = {
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
     // allow any type of linebreaks
-    'linebreak-style': 0
+    'linebreak-style': 0,
+    "no-console": 0,
+    "semi": 0,
+    "quotes": 0,
+    "comma-dangle": 0
+
   }
 }
