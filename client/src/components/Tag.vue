c7c9f970689c6487cef6e1c4fe2f8694998f792b
Michal Chwedczuk <czytelny@gmail.com>
2017-03-13 21:44:37 +0100

    listing users, tag component

---
+++ client/src/components/Tag.vue
@@ -0,0 +1,35 @@
+<template>
+  <span class="arij-tag" :class="{ special: isSpecial }">{{textMessage}}</span>
+</template>
+
+
+<script>
+  export default {
+    name: 'arij-tag',
+    props: ['textMessage', 'isSpecial']
+  };
+</script>
+
+<style>
+  .arij-tag {
+    background-color: #8391a5;
+    display: inline-block;
+    padding: 0 5px;
+    height: 22px;
+    line-height: 20px;
+    font-size: 12px;
+    color: #fff;
+    border-radius: 4px;
+    box-sizing: border-box;
+    border: 1px solid transparent;
+    white-space: nowrap;
+    margin-right: 1px;
+  }
+
+  .special {
+    background-color: rgba(253, 151, 31, 0.08);
+    border-color: #FD971F;
+    color: #FD971F;
+  }
+
+</style>
--- client/src/components/pages/settings/UsersSettingsPage.vue
+++ client/src/components/pages/settings/UsersSettingsPage.vue
@@ -1,12 +1,55 @@
 <template>
   <div>
-    <h3>users settings page</h3>
+    <h3>Users</h3>
+    <table class="u-full-width">
+      <thead>
+      <tr>
+        <th>Name</th>
+        <th>Nickname</th>
+        <th>Email</th>
+        <th>Roles</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="user in usersList">
+        <td>{{user.name}}</td>
+        <td>{{user.nickName}}</td>
+        <td>{{user.email}}</td>
+        <td>
+          <span v-for="role in user.roles">
+            <arij-tag
+              :text-message="role"
+              :isSpecial="isAdmin(role)"></arij-tag>
+          </span>
+        </td>
+      </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
+  import { FETCH_USERS } from './../../../store/action-types'
+  import Tag from './../../Tag'
+
   export default {
     name: 'users-settings-page',
+    beforeMount() {
+      this.$store.dispatch(FETCH_USERS)
+    },
+    computed: {
+      usersList() {
+        return this.$store.state.users.all;
+      }
+    },
+    methods: {
+      isAdmin(roleName) {
+        return roleName.toLowerCase() === 'admin';
+      }
+    },
+    components: {
+      'arij-tag': Tag
+    }
   };
 </script>
 
