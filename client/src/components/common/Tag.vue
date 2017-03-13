134f3d749b6bddbce7949d0f27740cd4fa4111e0
MPawluk <m.blizniak@gmail.com>
2017-03-13 21:49:10 +0100

    Added Tag Component

---
+++ client/src/components/common/Tag.vue
@@ -0,0 +1,25 @@
+<template>
+  <span class="a-tag">
+    <slot></slot>
+  </span>
+</template>
+<script>
+  export default {
+    name: 'a-tag',
+  };
+</script>
+<style lang="less" scope>
+  .a-tag {
+    background-color: #8391a5;
+    display: inline-block;
+    padding: 0 5px;
+    height: 24px;
+    line-height: 22px;
+    font-size: 12px;
+    color: #fff;
+    border-radius: 4px;
+    box-sizing: border-box;
+    border: 1px solid rgba(0, 0, 0, 0);
+    white-space: nowrap;
+  }
+</style>
--- client/src/components/pages/settings/ProjectsSettingsPage.vue
+++ client/src/components/pages/settings/ProjectsSettingsPage.vue
@@ -1,19 +1,40 @@
 <template>
   <div>
-      <h2>Projects settings page</h2>
-      <ul>
-        <li v-for="project in projects">
-          {{project.name}}
-        </li>
-      </ul>
+    <h2>Projects settings page</h2>
+    <table class="u-full-width">
+      <thead>
+      <tr>
+        <th>Name</th>
+        <th>Users</th>
+        <th>Updated</th>
+        <th></th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="project in projects">
+        <td>{{project.name}}</td>
+        <td>{{project.users_id}}</td>
+        <td>{{project.updated_at}}</td>
+        <td>
+          <a-tag>
+            {{project.active ? "Active" : "Inactive"}}
+          </a-tag>
+        </td>
+      </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-  import { FETCH_PROJECTS } from './../../../store/action-types'
+  import {FETCH_PROJECTS} from './../../../store/action-types'
+  import aTag from './../../common/Tag';
 
   export default {
     name: 'projects-settings-page',
+    components: {
+      'a-tag': aTag,
+    },
     computed: {
       projects() {
         return this.$store.state.projects.all;
