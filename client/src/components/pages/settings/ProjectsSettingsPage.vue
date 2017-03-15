4d4c9452170c7a4a462109bd01643367ff692c2c
MPawluk <m.blizniak@gmail.com>
2017-03-15 21:33:17 +0100

    [vip] Local Date filter is introduced

--- client/src/components/pages/settings/ProjectsSettingsPage.vue
+++ client/src/components/pages/settings/ProjectsSettingsPage.vue
@@ -12,9 +12,9 @@
       </thead>
       <tbody>
       <tr v-for="project in projects">
-        <td>{{project.name}}</td>
+        <td>{{project.name | date('DD-MM-YYYY')}}</td>
         <td>{{project.users_id}}</td>
-        <td>{{project.updated_at}}</td>
+        <td>{{project.updated_at | date('DD-MM-YYYY') }}</td>
         <td>
           <a-tag>
             {{project.active ? "Active" : "Inactive"}}
@@ -27,13 +27,24 @@
 </template>
 
 <script>
-  import {FETCH_PROJECTS} from './../../../store/action-types'
-  import aTag from './../../common/Tag';
+  import moment from 'moment';
+  import {FETCH_PROJECTS} from './../../../store/action-types';
+  import aTag from './../../Tag';
 
   export default {
     name: 'projects-settings-page',
     components: {
       'a-tag': aTag,
+    },
+    filters: {
+      date(value, pattern) {
+        console.info("weszlo!");
+        if (moment(value).isValid()) {
+          console.info("if!");
+          return moment(value, pattern);
+        }
+        return `${value}foo${pattern}`;
+      },
     },
     computed: {
       projects() {
